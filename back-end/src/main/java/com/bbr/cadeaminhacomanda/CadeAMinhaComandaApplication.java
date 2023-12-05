bf25196438579b77642970c54cad4b723bdd403e
henriquebenjamim <henriiqqq@gmail.com>
2023-12-04 22:41:23 -0300

    First configuration, folder organization

---
+++ back-end/src/main/java/com/bbr/cadeaminhacomanda/CadeAMinhaComandaApplication.java
@@ -0,0 +1,13 @@
+package com.bbr.cadeaminhacomanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadeAMinhaComandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadeAMinhaComandaApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/bbr/cadeaminhacomanda/CadeAMinhaComandaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbr.cadeaminhacomanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadeAMinhaComandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
